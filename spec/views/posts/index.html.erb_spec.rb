792f3a6d1fc184e972aed63ebefa645e88e10aec
Bruno Cardoso <bbc.bruno@yahoo.com>
2017-04-17 20:25:20 -0300

    First Commit

---
+++ spec/views/posts/index.html.erb_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "posts/index", type: :view do
+  before(:each) do
+    assign(:posts, [
+      Post.create!(
+        :title => "Title",
+        :body => "MyText",
+        :description => "MyText",
+        :slug => "Slug"
+      ),
+      Post.create!(
+        :title => "Title",
+        :body => "MyText",
+        :description => "MyText",
+        :slug => "Slug"
+      )
+    ])
+  end
+
+  it "renders a list of posts" do
+    render
+    assert_select "tr>td", :text => "Title".to_s, :count => 2
+    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+    assert_select "tr>td", :text => "Slug".to_s, :count => 2
+  end
+end
