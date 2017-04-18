eca44d90ec5747bdec64ee02fd5fe5de2759c10c
Bruno Cardoso <bbc.bruno@yahoo.com>
2017-04-18 01:16:56 -0300

    Generate image param for post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170417225454) do
+ActiveRecord::Schema.define(version: 20170418034234) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string "slug", null: false
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20170417225454) do
     t.string "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "banner_image_url"
     t.index ["slug"], name: "index_posts_on_slug", unique: true
   end
 
